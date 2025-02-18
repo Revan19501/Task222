5116b5d2c0711127ac1be8ff746c5a5ad718a309
Dinar Z <4.ak@bk.ru>
2025-02-18 22:31:47 +0300

    сделал таблицу чуть красивее

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,12 +1,13 @@
 package web.model;
 
 public class Car {
-    String brand;
-    String model;
-    int year;
+    private String brand;
+    private String model;
+    private int year;
 
     public Car() {
     }
+
     public Car(String brand, String model, int year) {
         this.brand = brand;
         this.model = model;
